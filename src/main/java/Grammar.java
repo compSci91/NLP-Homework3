100332fdafd3e590a2b8209b340df8fdc7bb4156
JoshuaHowell <howjosh@gmail.com>
2018-10-26 14:04:04 -0500

    JH -- Create Grammar from file.

--- src/main/java/Grammar.java
+++ src/main/java/Grammar.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Grammar {
@@ -7,6 +10,26 @@ public class Grammar {
 
     public Grammar(List<ProductionRule> productionRules){
         this.productionRules = productionRules;
+    }
+
+    public Grammar(File file){
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        productionRules = new ArrayList<ProductionRule>();
+
+        while (fileScanner.hasNextLine()) {
+            String productionString = fileScanner.nextLine();
+
+            ProductionRule newProduction = new ProductionRule(productionString);
+
+            productionRules.add(newProduction);
+        }
     }
 
     public boolean contains(ProductionRule productionRule){
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,29 +7,11 @@ public class Main {
 
         File file = new File("/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/pa3-cky/grammar_rules.txt");
 
-        Scanner fileScanner = null;
-        try {
-            fileScanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-        List<ProductionRule> productionRules = new ArrayList<ProductionRule>();
-
-        while (fileScanner.hasNextLine()) {
-            String productionString = fileScanner.nextLine();
-
-            ProductionRule newProduction = new ProductionRule(productionString);
-
-            productionRules.add(newProduction);
-        }
-
-        Grammar grammar = new Grammar(productionRules);
+        Grammar grammar = new Grammar(file);
 
         System.out.println(grammar);
 
 
-
     }
+
 }
