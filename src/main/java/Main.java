639b4f948eb2d39d974388588c752451591872af
JoshuaHowell <howjosh@gmail.com>
2018-10-27 16:38:03 -0500

    JH -- Final commit. Make code work for multiple sentences in a file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //4b64d699-5b90-4150-a1c4-d3d3b8b12a5b
 
-        File grammarFile = new File("/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/pa3-cky/grammar_rules.txt");
-       // File grammarFile = new File(args[0]);
+        //File grammarFile = new File("/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/pa3-cky/grammar_rules.txt");
+         File grammarFile = new File(args[0]);
 
 
-        File sentsFile = new File("/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/pa3-cky/sents.txt");
+        //File sentsFile = new File("/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/pa3-cky/sents.txt");
 
-       // File sentsFile = new File(args[1]);
+        File sentsFile = new File(args[1]);
 
         Grammar grammar = new Grammar(grammarFile);
 
@@ -22,17 +22,27 @@ public class Main {
             e.printStackTrace();
         }
 
-        List<String> words = new ArrayList<String>();
 
-        while(sentsScanner.hasNext()){
-            words.add(sentsScanner.next());
-        }
+        while (sentsScanner.hasNextLine()) {
+                String sentence = sentsScanner.nextLine();
 
-        ProductionRuleList[][] productionRules = new Parser().ckyParse(words, grammar);
+                Scanner sentenceScanner = new Scanner(sentence);
+
+                List<String> words = new ArrayList<String>();
+
+            while (sentenceScanner.hasNext()) {
+                words.add(sentenceScanner.next());
+            }
+
+            ProductionRuleList[][] productionRules = new Parser().ckyParse(words, grammar);
 
 
-        new Printer().print(productionRules, words);
+            new Printer().print(productionRules, words);
 
+            System.out.println();
+            System.out.println("**********");
+            System.out.println();
+    }
 
 
     }
