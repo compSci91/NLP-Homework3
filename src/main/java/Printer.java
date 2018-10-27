611a733bfd361c95396b27bcddf785e8349438d1
JoshuaHowell <howjosh@gmail.com>
2018-10-27 14:41:36 -0500

    JH -- Print the entire table.

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,13 +1,25 @@
 import java.util.List;
 
 public class Printer {
-    public void print(ProductionRuleList[][] productionRules, List<String> words){
-
-        for(int i = 0; i < words.size(); i++){
-            System.out.println("SPAN: " + words.get(i));
-            System.out.println(productionRules[i][i+1]);
+    public void print(ProductionRuleList[][] productionRules, List<String> words) {
 
 
+        for (int lengthOfSpan = 1; lengthOfSpan <= words.size(); lengthOfSpan++) {
+            for (int beginOfSpan = 0; beginOfSpan < words.size(); beginOfSpan++) {
+                int endOfSpan = beginOfSpan + lengthOfSpan;
+
+                if(endOfSpan < words.size() + 1) {
+
+                    System.out.print("SPAN: ");
+                    for (int i = beginOfSpan; i < beginOfSpan + lengthOfSpan; i++) {
+                        System.out.print(words.get(i) + " ");
+                    }
+
+                    System.out.println();
+                    System.out.println(productionRules[beginOfSpan][beginOfSpan + lengthOfSpan]);
+
+                }
+            }
         }
 
 
